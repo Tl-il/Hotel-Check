67bed2586813d12eb51e28a96cd3cb73a0d4824a
noaeizenberg <noaeizenberg12@cs.colman.ac.il>
2024-05-10 18:35:25 +0300

    main

--- app/navigation/BarBottom.js
+++ app/navigation/BarBottom.js
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
 import HomeScreen from '../Screen/HomeScreen';
 import AccountScreen from '../Screen/AccountScreen';
+import LoginScreen from '../Screen/LoginScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -9,6 +10,7 @@ const BarBottom = () => (
     <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Account" component={AccountScreen} />
+        <Tab.Screen name="Login" component={LoginScreen} />
     </Tab.Navigator>
 )
 export default BarBottom;

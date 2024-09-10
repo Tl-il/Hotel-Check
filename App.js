1300dd10b3b1373ded32d6026ea12730e34771c2
Tamir <t0548048331@gmail.com>
2024-09-10 20:12:50 +0300

    main

--- App.js
+++ App.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-=======
 
 import {TextInput,StyleSheet, Text, View, Image, SafeAreaView, StatusBar, Platform, Button, Dimensions, useWindowDimensions } from 'react-native';
 import { firebaseConfig } from "./app/firebase";
->>>>>>> 67ba0d3f (main)
 import { NavigationContainer } from '@react-navigation/native';
+import AuthNavigator from './app/navigation/AuthNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
-<<<<<<< HEAD
-import WelcomeNavigator from './app/navigation/WelcomeNavigator';
-
-export default function App() {
-  
-  return (
-   
-=======
 import BarBottom from './app/navigation/BarBottom';
 import WelcomeNavigator from './app/navigation/WelcomeNavigator';
 
@@ -25,15 +15,11 @@ export default function App() {
   return (
   
 
->>>>>>> 67ba0d3f (main)
     <NavigationContainer theme={navigationTheme}>
       <WelcomeNavigator/>
     </NavigationContainer>
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 67ba0d3f (main)
   );
 }
 
